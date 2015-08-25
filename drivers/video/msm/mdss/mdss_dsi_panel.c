ea96167e690203130c5072eeedc0baee575b15cc
Ryan Andri <ryan.omnia@gmail.com>
2015-08-25 13:37:36 +0700

    mdss: add powersuspend hook
    Signed-off-by: Ryan Andri <ryan.omnia@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -28,6 +28,9 @@
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <mach/mmi_panel_notifier.h>
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
 
 #include "mdss_dsi.h"
 #include "mdss_fb.h"
@@ -743,6 +746,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -895,6 +902,10 @@ disable_regs:
 		pdata->panel_info.cabc_mode = CABC_OFF_MODE;
 
 	pr_info("%s-:\n", __func__);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 	return 0;
 }
